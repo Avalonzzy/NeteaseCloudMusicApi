2bfcd931d3e2944d781bbb1b5a6fbcecb707425e
Avalonzzy <59643286+Avalonzzy@users.noreply.github.com>
2023-04-23 20:37:18 +0800

    Update lyric.js
    weapi貌似在获取歌词方面出现问题，经过测试，改成api即可

--- module/lyric.js
+++ module/lyric.js
@@ -5,7 +5,7 @@ module.exports = (query, request) => {
     id: query.id
   }
   return request(
-    'POST', `https://music.163.com/weapi/song/lyric?lv=-1&kv=-1&tv=-1`, data,
+    'POST', `https://music.163.com/api/song/lyric?lv=-1&kv=-1&tv=-1`, data,
     {crypto: 'linuxapi', cookie: query.cookie, proxy: query.proxy}
   )
 }
